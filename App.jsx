13c7311779900bec59e0e72a964e581453457aff
Hamza21u <hamzamukhtarpk21@gmail.com>
2024-09-03 17:04:02 +0500

    Home Page

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home'; 
 import AddDestinations from './src/screens/AddDestinations'; 
-import MapView from 'react-native-maps';
+
 import MapViews from './src/screens/MapViews'
 import DestinationDetails from './src/screens/DestinationDetails'
 
